c76eb561c7602f896fe98d8e57d04b5ddf46649e
AnuragSPDev <92988001+AnuragSPDev@users.noreply.github.com>
2021-10-27 15:11:52 +0530

    Add files via upload

--- udacity_data.py
+++ udacity_data.py
@@ -1,25 +1,45 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+import traceback
 
-url = requests.get('https://www.udacity.com/school-of-data-science', verify=False)
+try:
+    url = requests.get('https://www.udacity.com/school-of-data-science', verify=False)
+    url.raise_for_status()
+    soup = bs(url.content, 'html.parser')
+    try:
+        data = soup.find('div', class_='upcoming-programs-list_layout__28HUd')
+        # print(len(data))
 
-soup = bs(url.content, 'html.parser')
+        u_list = data.find('ul')
 
+        main_di = {}
+        i = 0
+        for item in u_list.children:
+            di = {}
+            di['start_date'] =item.find_all('div')[0].find('time').get_text()
+            di['heading'] = item.find_all('div')[1].find('h3').get_text()
+            di['concepts'] = item.find_all('div')[1].find('p').get_text()
+            main_di[i] = di
+            i += 1
 
+        with open('course_data.json', 'w') as fp:
+            json.dump(main_di, fp, indent=4)
+    except Exception as e:
+        print('Exception Occurred')
+        print('{}'.format(traceback.format_exc()))
 
-data = soup.find('div', class_='upcoming-programs-list_layout__28HUd')
-u_list = data.find('ul')
+# Connection error
+except requests.ConnectionError as e:
+    print('{}'.format(e))
 
-main_di = {}
-i = 0
-for item in u_list.children:
-    di = {}
-    di['start_date'] =item.find_all('div')[0].find('time').get_text()
-    di['heading'] = item.find_all('div')[1].find('h3').get_text()
-    di['concepts'] = item.find_all('div')[1].find('p').get_text()
-    main_di[i] = di
-    i += 1
+# http status not OK
+except requests.exceptions.HTTPError as e:
+    print('{}'.format(e))
 
-with open('course_data.json', 'w') as fp:
-    json.dump(main_di, fp, indent=4)
+# invalis url
+# except requests.exception.URLError as e:
+#     print('{} - URL not found'.format(e))
+
+# except requests.exceptions.RequestException as e:
+#     print('{}'.format(e))
